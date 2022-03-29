4a001e0e0df78a7c89e7d139147025da81132b86
Kubatbekkk <kubineccastro@gmail.com>
2022-03-30 01:02:43 +0600

    Initial and Final Commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,30 +9,33 @@ import {
   Typography,
 } from "@material-ui/core";
 import { ShoppingCart } from "@material-ui/icons";
+import { Link, useLocation } from 'react-router-dom';
 
-import logo from '../../assets/commerce.png';
+import logo from '../../assets/camera.png';
 
 import useStyles from './styles';
 
-const Navbar = () => {
+const Navbar = ({ totalItems }) => {
     const classes = useStyles();
+    const location = useLocation();
 
   return (
     <>
       <AppBar position="fixed" className={classes.AppBar} color="inherit">
         <Toolbar>
-            <Typography variant="h6" className={classes.title} color="inherit">
+            <Typography component={Link} to="/" variant="h6" className={classes.title} color="inherit">
                 <img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
-                Commerce.js
+                Adorama Online Store
             </Typography>
             <div className={classes.grow} />
+            {location.pathname === '/' && (
             <div className={classes.button}>
-                <IconButton aria-label="Show cart items" color="inherit">
-                    <Badge badgeContent={2} color="secondary">
+                <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+                    <Badge badgeContent={totalItems} color="secondary">
                         <ShoppingCart />
                     </Badge>
                 </IconButton>
-            </div>
+            </div>)}
         </Toolbar>
       </AppBar>
     </>
